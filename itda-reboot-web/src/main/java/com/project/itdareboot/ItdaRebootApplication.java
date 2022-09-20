2e8b5bd8cba7e592ccb8b69e8ddef0967a6b2aec
Hyuntae <bht9011@gmail.com>
2022-09-20 22:18:21 +0900

    initial commit

---
+++ itda-reboot-web/src/main/java/com/project/itdareboot/ItdaRebootApplication.java
@@ -0,0 +1,13 @@
+package com.project.itdareboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItdaRebootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItdaRebootApplication.class, args);
+    }
+
+}
---
+++ itda-reboot-web/src/test/java/com/project/itdareboot/ItdaRebootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.itdareboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItdaRebootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
